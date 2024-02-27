5ff79d969bc5a6d67fd44b8e48f47b1576afe560
MassimoRomano <romano93m@gmail.com>
2024-02-28 00:30:22 +0100

    fix error

--- pari e dispari/js/main.js
+++ pari e dispari/js/main.js
@@ -1,5 +1,5 @@
 // Ottieni la scelta dell'utente (trasformata in minuscolo per uniformità)
-const  userSelect= prompt('Pari o dispari').toLowerCase();
+const userSelect= prompt('Pari o dispari').toLowerCase();
 
 // Ottieni un numero dall'utente compreso tra 1 e 5
 const userChoice = parseInt(prompt('Scegli un numero da 1 a 5'));
@@ -14,7 +14,7 @@ console.log(pcChoice);
 
 
 // Calcola la somma dei numeri dell'utente e del computer
-let sum = userNumber + pcNumber;
+let sum = userChoice + pcChoice;
 // Stampa la somma
 console.log(sum);
 
@@ -23,15 +23,18 @@ let message = "";
 
 
 // Funzione per determinare il risultato del gioco (se l'utente ha vinto o perso)
-function gameEvenOrOdd(userChoice, userNumber) {
+function game(userSelect, userChoice) {
      // se la somma dei numeri e pari l'utente ha vinto
-     if (sum % 2 == 0 && userChoice == 'pari') {
+     if (sum % 2 == 0 && userSelect == 'pari') {
         return message = 'Hai vinto!';
     // Se la somma dei numeri e dispari e l'utente ha scelto dispari ha vinto
-    } else if (sum % 2 ==- 1 && userChoice == 'dispari') {
+    } else if (sum % 2 ==- 1 && userSelect == 'dispari') {
         return message = 'Hai vinto!';
     // Altrimenti, l'utente perde
     } else {
         return message = 'Hai perso!';
     }
 }
+
+// Chiama la funzione per determinare il risultato del gioco e visualizza un alert con il risultato
+alert(game(userSelect, userChoice));
