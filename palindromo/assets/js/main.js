25e0ab57c16187208b373473692f5731fe8492ed
MassimoRomano <romano93m@gmail.com>
2024-02-27 17:59:49 +0100

    add fuction to verify

--- palindromo/assets/js/main.js
+++ palindromo/assets/js/main.js
@@ -1,2 +1,11 @@
 // Chiedo all'utente di inseire una parola
 const word = prompt("Inserisci una parola");
+
+//definisco la funzione per verificare se la parola inserita dall'utente sia un palindromo
+function isPal(word){
+
+
+//aggiungo una variabile vuota per poi inseire la parola invertita
+    let invertWord = '';
+
+}
