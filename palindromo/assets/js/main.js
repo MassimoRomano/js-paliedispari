df0e99dfe631f42bfaaef6f4184be6ad84c2821d
MassimoRomano <romano93m@gmail.com>
2024-02-27 18:06:09 +0100

    add cycle for

--- palindromo/assets/js/main.js
+++ palindromo/assets/js/main.js
@@ -7,5 +7,10 @@ function isPal(word){
 
 //aggiungo una variabile vuota per poi inseire la parola invertita
     let invertWord = '';
+    
 
+//avvio un ciclo for per invertire la parola
+for(let i = 0 ; word.lenght -1 ; i >= 0 ; i-- ){
+    invertWord += word[i];
+}
 }
